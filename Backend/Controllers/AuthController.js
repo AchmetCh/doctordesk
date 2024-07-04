0ec97f8b66b2782e5991beb1f92bce829df45ea4
achmet <gigsakos@gmail.com>
2024-07-04 21:37:56 +0300

    backend is working and tested

--- Backend/Controllers/AuthController.js
+++ Backend/Controllers/AuthController.js
@@ -1,4 +1,4 @@
-const Doctor = require('../Models/User')
+const {User} = require('../Models/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
@@ -7,12 +7,13 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 
 exports.register = async (req,res) => {
-    const {username, passowrd} = req.body
     try {
-        const user = await Doctor.findOne({username})
+        const user = await User.findOne({username: req.body.username})
         if(user) return res.status(400).json({msg: 'User already exists'})
-            const password = await bcrypt.hash(passowrd, SALT_ROUNDS)
-        const newUser = new Doctor({username, password})
+        const password = await bcrypt.hash(req.body.password, SALT_ROUNDS)
+        const newUser = new User({
+            username: req.body.username, 
+            password})
         await newUser.save()
         res.json({msg: 'User registered successfully'})
         } catch (error) {
@@ -22,15 +23,16 @@ exports.register = async (req,res) => {
             exports.login = async (req,res) => {
                 const {username, password} = req.body
                 try {
-                    const user = await Doctor.findOne({username})
+                    const user = await User.findOne({username})
                     if(!user) return res.status(400).json({msg: 'User does not exist'})
                         const isMatch = await bcrypt.compare(password, user.password)
                     if(!isMatch) return res.status(400).json({msg: 'Invalid credentials'})
                         const payload = {user: {id: user.id}}
-                    const token = jwt.sign(payload, PRIVATE_KEY, {expiresIn: 360000})
+                    const token = jwt.sign(payload, PRIVATE_KEY, {expiresIn: '360000'})
                     res.status(200).send({token, user:{id:user._id}})
                     } catch (error) {
                         console.log(error)
                         res.status(500).send({message:'Unable to login'})
                         }
 }
+
--- Backend/Controllers/Controllers.js
+++ Backend/Controllers/Controllers.js
@@ -1,4 +1,4 @@
-const Patient = require('../Models/Patient')
+const {Patient} = require('../Models/Patient')
 
 exports.showAllPatient = async(req, res) => {
     try {
@@ -9,21 +9,35 @@ exports.showAllPatient = async(req, res) => {
             }
 }
 
-exports.searchPatient = async(req, res) => {
+exports.createPatient = async (req, res) => {
     try {
-        const {ssn} = req.params
-        const patient = await Patient.findOne({ ssn })
-        if (!patient) return res.status(404).json({ message: "Patient not found"})
-            res.status(200).json(patient)
+        const patient = await Patient.create(req.body)
+        res.status(201).json(patient)
         } catch (error) {
-            res.status(500).json({ message: error.message })
-        }
+            res.status(500).json(error)
+            }
 }
+
+exports.searchPatient = async (req, res) => {
+    try {
+        const SSN = req.params.ssn;
+        const patient = await Patient.findOne({ SSN });
+
+        if (!patient) {
+            return res.status(404).json({ message: "Patient not found" });
+        }
+
+        res.status(200).json(patient);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 exports.updatePatient = async(req, res) => {
     try {
-        const {ssn} = req.params
-        const patient = await Patient.findOneAndUpdate({ ssn }, req.body, { new: true })
+        const SSN = req.params.ssn
+        console.log(`patient SSN ${SSN}`);
+        const patient = await Patient.findOneAndUpdate({ SSN }, req.body, { new: true })
         if (!patient) return res.status(404).json({ message: "Patient not found"})
             res.status(200).json(patient)
             } catch (error) {
@@ -33,10 +47,10 @@ exports.updatePatient = async(req, res) => {
 
 exports.deletePatient = async (req,res) => {
     try {
-        const {ssn} = req.params
-        const patient = await Patient.findOneAndDelete({ ssn })
+        const SSN = req.params.ssn
+        const patient = await Patient.findOneAndDelete({ SSN })
         if (!patient) return res.status(404).send({ message: "Patient not found"})
-            res.status(200).json(patient)
+            res.status(200).send('Patient deleted successfully', patient)
         } catch (error) {
             res.status(500).json({ message: error.message })
             }
