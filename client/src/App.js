dd229e86333c06e9a184c01fdddd0d110163d958
achmet <gigsakos@gmail.com>
2024-07-06 00:13:40 +0300

    Navbar, logout, login ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import React, {useState} from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import SearchPatient from './components/SearchPatient';
 
 function App() {
+  const isLoggedIn = !!localStorage.getItem('token')
+
   return (
-    <div className="App">
-     <h1>dream team is ready</h1>
-    </div>
+<Router>
+      <div className="App"> 
+        {isLoggedIn ? (
+          <>
+            <NavBar className="layout" />
+            <Routes>
+              <Route path="/searchpatient" element={<SearchPatient />} />
+              <Route index element={<SearchPatient />} />
+              {/* Add more routes here */}
+            </Routes>
+          </>
+        ) : (
+          <Routes>
+           <Route path="/" element={<Login />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        )}
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
