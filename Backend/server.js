659caab7b5c5a969628b6d648d2de154f99fccf6
Achmet <gigsakos@gmail.com>
2025-07-04 21:25:54 +0300

    created env

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ app.use("/api", patientRoutes);
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
 
